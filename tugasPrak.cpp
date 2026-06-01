0c223e8cf63bf9d441aa7bfa3ee681451a80d0fd
dwirahmanvalensa-del <dwirahmanvalensa@gmail.com>
2026-06-01 19:53:08 +0800

    menambahkan fungsi CRUD

--- tugasPrak.cpp
+++ tugasPrak.cpp
@@ -37,3 +37,63 @@ public:
     }
 };
 
+void readGudang() {
+    ifstream infile("gudang.txt");
+    string baris;
+    cout << "\n=== DAFTAR BARANG DI GUDANG ===" << endl;
+    int i = 1;
+    while (getline(infile, baris)) {
+        cout << i++ << ". " << baris << endl;
+    }
+    infile.close();
+}
+
+void createBarang() {
+    ofstream outfile("gudang.txt", ios::app);
+    string barang;
+    cout << "Nama barang baru: "; 
+    cin.ignore(); getline(cin, barang);
+    outfile << barang << endl;
+    outfile.close();
+    cout << "Berhasil ditambah!" << endl;
+}
+
+void updateBarang() {
+    ifstream infile("gudang.txt");
+    string data[100], baris;
+    int n = 0;
+    while (getline(infile, baris)) data[n++] = baris;
+    infile.close();
+
+    int pilih;
+    cout << "Pilih nomor yang diupdate: "; 
+    cin >> pilih;
+    if (pilih > 0 && pilih <= n) {
+        cout << "Nama baru: "; 
+        cin.ignore(); getline(cin, data[pilih-1]);
+        
+        ofstream outfile("gudang.txt");
+        for (int i = 0; i < n; i++) outfile << data[i] << endl;
+        outfile.close();
+        cout << "Berhasil diupdate!" << endl;
+    }
+}
+
+void deleteBarang() {
+    ifstream infile("gudang.txt");
+    string data[100], baris;
+    int n = 0;
+    while (getline(infile, baris)) data[n++] = baris;
+    infile.close();
+
+    int pilih;
+    cout << "Pilih nomor yang dihapus: "; 
+    cin >> pilih;
+    if (pilih > 0 && pilih <= n) {
+        ofstream outfile("gudang.txt");
+        for (int i = 0; i < n; i++) if (i != pilih-1) outfile << data[i] << endl;
+        outfile.close();
+        cout << "Berhasil dihapus!" << endl;
+    }
+}
+
