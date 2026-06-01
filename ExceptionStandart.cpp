3383231d375bc8d339cd44cfeb262536666019d3
dwirahmanvalensa-del <dwirahmanvalensa@gmail.com>
2026-06-01 16:18:05 +0800

    mendeklarasikan objek array data dan output  elemen ke-5

--- ExceptionStandart.cpp
+++ ExceptionStandart.cpp
@@ -9,4 +9,9 @@ int main()
     cout << "Awal program" << endl; //penanda 1
     try
     {
-        
+        //deklarasi array data 
+        array <int, 3> data = { 1, 2, 3 };
+        //memanggil array element ke-5 yang berarti akan terjadi error
+        cout << data.at(5) << endl;
+    }
+    
