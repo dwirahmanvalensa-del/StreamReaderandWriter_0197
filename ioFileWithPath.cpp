8961f8ea3ccae3fbc24aa4d67d1ba416fdb9bae6
dwirahmanvalensa-del <dwirahmanvalensa@gmail.com>
2026-06-01 16:23:45 +0800

    mendeklarasikan variabel dan menambahkan input nama file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -5,4 +5,11 @@ using namespace std;
 
 int main()
 {
+    //deklarasi variabel 
+    string baris, NamaFile;
+
+    //membuat input untuk nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
+
     
